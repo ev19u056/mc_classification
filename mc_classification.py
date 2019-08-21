0e206204f21e01aba9e0a9d2c38a16a66c97194d
ev19u056 <dmytro.ostapchuk@ua.pt>
2019-08-21 16:10:16 +0100

    prepareData

--- mc_classification.py
+++ mc_classification.py
@@ -44,7 +44,6 @@ if __name__ == "__main__":
 
     ## Input arguments. Pay speciall attention to the required ones.
     parser = argparse.ArgumentParser(description='Process the command line options')
-    parser.add_argument('-z', '--batch', action='store_true', help='Whether this is a batch job, if it is, no interactive questions will be asked and answers will be assumed')
     parser.add_argument('-v', '--verbose', action='store_true', help='Whether to print verbose output')
     parser.add_argument('-e', '--epochs', type=int, required=True, help='Number of epochs')
     parser.add_argument('-a', '--batchSize', type=int, required=True, help='Batch size')
@@ -55,10 +54,12 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--iteration', type=int, default=1, help='Iteration number i')
     parser.add_argument('-f', '--fraction', type=float, default=0.3, help="The fraction of available data to be loaded")
     parser.add_argument('-l', '--List', type=str, required=True, help='Defines the architecture of the NN; e.g: -l "14 12 7" -> 3 hidden layers of 14, 12 and 7 neurons respectively (input always 53, output always 1)')
+
     parser.add_argument('-act', '--act', type=str, default="relu", help='activation function for the hidden neurons')
     parser.add_argument('-ini', '--initializer', type=str, default="he_normal", help='Kernel Initializer for hidden layers')
     parser.add_argument('-bN', '--batchNorm', action='store_true',help='Wether to use Batch Normalization')
-
+    parser.add_argument('-es', '--EarlyStopping', action='store_true',help='Wether to use EarlyStopping Callback')
+    parser.add_argument('-rlrop', '--ReduceLROnPlateau', action='store_true',help='Wether to use ReduceLROnPlateau Callback')
     args = parser.parse_args()
 
     n_epochs = args.epochs
@@ -141,10 +142,20 @@ if __name__ == "__main__":
     # Compile
     model.compile(**compileArgs)
 
+    callbacks = []
     lrm = LearningRateMonitor()
-    callbacks = [EarlyStopping(patience=15, verbose=True),
-                    ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=True, cooldown=1, min_lr=0), # argument min_delta is not supported
-                    ModelCheckpoint(filepath+name+".h5", save_best_only=True, save_weights_only=True), lrm]
+    ModelCheckpoint = ModelCheckpoint(filepath+name+".h5", save_best_only=True, save_weights_only=True)
+    callbacks.append(lrm)
+    callbacks.append(ModelCheckpoint)
+
+    if args.EarlyStopping:
+        EarlyStopping = EarlyStopping(patience=15, verbose=True)
+        callbacks.append(EarlyStopping)
+
+    if args.ReduceLROnPlateau:
+        ReduceLROnPlateau = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=True, cooldown=1, min_lr=0), # argument min_delta is not supported
+        callbacks.append(ReduceLROnPlateau)
+
     # Fitting the Model -> TRAINING
     # fit(x=None, y=None, batch_size=None, epochs=1, verbose=1, callbacks=None, validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None, validation_freq=1)
     history = model.fit(XDev, YDev, validation_data=(XVal,YVal,weightVal),sample_weight=weightDev,shuffle=True,callbacks=callbacks, **trainParams)
@@ -253,28 +264,27 @@ if __name__ == "__main__":
     print("DONE: Creating a text file where all of the model's caracteristics are displayed")
 
     # Plot accuracy and loss evolution over epochs for both training and validation datasets
-    if not args.batch:
-        fig=plt.figure()
-        plt.subplots_adjust(hspace=0.5)
+    fig=plt.figure()
+    plt.subplots_adjust(hspace=0.5)
 
-        plt.subplot(2,1,1)
-        plotter(filepath+"accuracy/acc_"+name+".pickle","accuracy",name+"'s accuracy")
-        plotter(filepath+"accuracy/val_acc_"+name+".pickle","Val accuracy",name+"'s Accuracy")
-        plt.grid()
-        plt.legend(['train', 'val'], loc='upper left')
-        #plt.savefig(filepath+"accuracy/Accuracy.pdf")
+    plt.subplot(2,1,1)
+    plotter(filepath+"accuracy/acc_"+name+".pickle","accuracy",name+"'s accuracy")
+    plotter(filepath+"accuracy/val_acc_"+name+".pickle","Val accuracy",name+"'s Accuracy")
+    plt.grid()
+    plt.legend(['train', 'val'], loc='upper left')
+    #plt.savefig(filepath+"accuracy/Accuracy.pdf")
 
-        plt.subplot(2,1,2)
-        plotter(filepath+"loss/loss_"+name+".pickle","loss",name +"loss function")
-        plotter(filepath+"loss/val_loss_"+name+".pickle","loss Validation",name+"'s Loss")
-        plt.grid()
-        plt.legend(['train', 'val'], loc='upper left')
-        #plt.savefig(filepath + "loss/Loss_Validation.pdf")
+    plt.subplot(2,1,2)
+    plotter(filepath+"loss/loss_"+name+".pickle","loss",name +"loss function")
+    plotter(filepath+"loss/val_loss_"+name+".pickle","loss Validation",name+"'s Loss")
+    plt.grid()
+    plt.legend(['train', 'val'], loc='upper left')
+    #plt.savefig(filepath + "loss/Loss_Validation.pdf")
 
-        plt.savefig(filepath+name+"_Accuracy_Loss_"+compileArgs['loss']+".pdf")
-        plt.close()
+    plt.savefig(filepath+name+"_Accuracy_Loss_"+compileArgs['loss']+".pdf")
+    plt.close()
 
-        if args.verbose:
-            print("Accuraccy and loss plotted at {}".format(filepath))
-            print ("Model name: "+name)
-        sys.exit("Done!")
+    if args.verbose:
+        print("Accuraccy and loss plotted at {}".format(filepath))
+        print ("Model name: "+name)
+    sys.exit("Done!")
